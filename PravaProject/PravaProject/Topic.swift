9e99a5ee46065a17ffdce2d11d19bec9e7f1c9c7
slezh12 <slezh12@freeuni.edu.ge>
2016-01-09 07:37:14 -0800

    Model classes

--- PravaProject/PravaProject/Topic.swift
+++ PravaProject/PravaProject/Topic.swift
@@ -1,5 +1,5 @@
 //
-//  User.swift
+//  Topic.swift
 //  PravaProject
 //
 //  Created by Sandro on 1/9/16.
@@ -9,10 +9,9 @@
 import Foundation
 
 
-public class User {
-    
+public class Topic {
+
     var ID: Int!
-    var name: String!
-    var password: String!
+    var topic: String!
     
 }
