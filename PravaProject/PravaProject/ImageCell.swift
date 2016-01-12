b4a7e93dc043e78974512700c6ded0ec1feea9ee
kokotagauri <kokotagauri@Kokos-Mac.local>
2016-01-12 09:20:31 -0800

    ImageCell, MainStryboard, MistakesController

--- PravaProject/PravaProject/ImageCell.swift
+++ PravaProject/PravaProject/ImageCell.swift
@@ -11,4 +11,42 @@ import UIKit
 class ImageCell: UITableViewCell {
     @IBOutlet weak var questionImage: UIImageView!
 
+    var imagePath: String? {
+        didSet {
+            questionImage.image = nil
+            if let path = imagePath {
+                let qos = Int(QOS_CLASS_USER_INITIATED.rawValue)
+                dispatch_async(dispatch_get_global_queue(qos, 0)) {
+                    
+                    dispatch_async(dispatch_get_main_queue()) {
+                        if path == self.imagePath {
+                            self.questionImage.image = self.loadImageFromPath(path)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func loadImageFromPath(path: String) -> UIImage? {
+        
+        let image = UIImage(contentsOfFile: path)
+        
+        if image == nil {
+            
+            print("missing image at: \(path)")
+        } else {
+            
+            print("Loading image from path: \(path)")
+            
+        }
+        return image
+        
+    }
+
+    
 }
--- PravaProject/PravaProject/MistakesController.swift
+++ PravaProject/PravaProject/MistakesController.swift
@@ -9,5 +9,92 @@
 import UIKit
 
 class MistakesController: UITableViewController {
+    
+    var errors : [Error] = [Error]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.estimatedRowHeight = tableView.rowHeight
+        tableView.rowHeight = UITableViewAutomaticDimension
+        let app = UIApplication.sharedApplication().delegate as! AppDelegate
+        app.user = DBHelper.getDBHelper().selectUser(1)
+        errors = DBHelper.getDBHelper().selectErrorsWithQuestionsByUserID(app.user!.ID)
+        self.tableView.reloadData()
+    }
+    
+    var curr = 1
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return self.errors.count
+    }
+    
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        return UITableViewAutomaticDimension
+        
+    }
+    
+    
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        curr = errors[indexPath.row].ID
+        
+    }
+    
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("mistakeCell", forIndexPath: indexPath)
+        
+        let currScore = errors[indexPath.row]
+        
+        cell.textLabel?.text = currScore.question.description
+        return cell
+    }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if segue.identifier == nil { return }
+        if(segue.identifier == "StartErrorSegue"){
+            if let gamecontroller = segue.destinationViewController as? GameController {
+                var q = [Question]()
+                for(_,element) in errors.enumerate() {
+                    q.append(element.question)
+                }
+                gamecontroller.questions = q
+            }
+        }
+        
+        if(segue.identifier == "QuestionSegue"){
+            if let questioncontroller = segue.destinationViewController as? QuestionController {
+                let q = errors[curr].question
+                
+                questioncontroller.question = q
+                
+                print(q.description + " yleeeee")
+            }
 
+        }
+    }
 }
